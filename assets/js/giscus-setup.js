a934e59529a29146d55d6c62f57c675efc80ebdd
devrisler <devris.isler@gmail.com>
2025-12-16 17:11:58 +0000

    Deploying to gh-pages from @ devrisler/devrisler.github.io@51f71789ffeace553827d07ad5b259d145c15c3a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "devrisler/devris.isler",
+    "data-repo": "devrisler/devrisler.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
